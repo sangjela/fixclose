a3790e2efe1e2c7fd5ff05353d8060541d48cc02
Woo Sangje <sangjela@gmail.com>
2024-01-23 07:02:16 +0900

    Add Test html, doc and UA info

--- src/assets/closeUtil.js
+++ src/assets/closeUtil.js
@@ -1,4 +1,18 @@
 // 自windowを閉じる
 function windowCloseForce(){
-  open('about:blank', '_self').close();    //一度再表示してからClose
+
+  let ua = navigator.userAgent;
+  console.log("UA = ", ua);
+
+  if( ua.indexOf("Chrome") >= 0 ){
+    console.log("go chrome end");
+    console.log("window.self is ", window.self);
+    console.log("window.top is ", window.top);
+  }else{
+    //ie case?
+    console.log("go try close");
+    console.log("window.self is ", window.self);
+    console.log("window.top is ", window.top);
+    open('about:blank', '_self').close(); //一度再表示してからClose
+  }
 }

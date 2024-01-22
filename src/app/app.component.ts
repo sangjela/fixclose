37b9909f88a45597249b0bfbca7d0340a9da6c13
Woo Sangje <sangjela@gmail.com>
2024-01-23 03:16:43 +0900

    make first compo for init > close

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'fixclose';
+
+  public showHi(): void {
+    //alert('Hi EveryBody');
+    window.close();
+  }
 }
